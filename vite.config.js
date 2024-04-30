b0f721e925d828e68414eaa4c9a29bdb211d7e61
Chukwuemeka Chinemelu <wikiwoo@MacBook-Air.local>
2024-04-30 11:00:47 +0100

    Updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the warning limit as needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example manualChunks configuration
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
